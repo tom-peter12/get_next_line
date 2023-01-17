ce8df6b7ec44117f6fc5773b456a26f7aee975f2
Tomas Petros <tpetros@lab1r4s13.42abudhabi.ae>
2023-01-17 23:07:52 +0400

    Project starter

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetros <tpetros@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 23:01:59 by tpetros           #+#    #+#             */
+/*   Updated: 2023/01/17 23:02:01 by tpetros          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
